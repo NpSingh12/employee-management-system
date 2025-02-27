9052526ef481bfb0fc78b676762ea77398641e5e
NpSingh12 <narinderpals404@gmail.com>
2025-02-27 14:28:50 +0530

    make it responsive

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import  tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base:"/employment_management_system"
+  base: "/employee-management-system/", // Corrected base path
+
 })
